6fc3c0519b5b89e25d40f3a9cf3a1ee7889a6180
Ivan Volodartsev <untrupus@gmail.com>
2020-08-29 00:03:31 +0600

    add edit contacts option

--- src/containers/Contacts/Contacts.js
+++ src/containers/Contacts/Contacts.js
@@ -4,6 +4,7 @@ import axiosOrders from "../../axiosOrders";
 
 const Contacts = () => {
     const [contacts, setContacts] = useState({});
+    const [showEdit, setShowEdit] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -14,6 +15,22 @@ const Contacts = () => {
         fetchData().catch(console.error);
     }, []);
 
+    const changeContacts = event => {
+        const name = event.target.name;
+        const value = event.target.value;
+        const newContacts = {...contacts};
+        newContacts[name] = value;
+        setContacts(newContacts);
+    }
+
+    const saveChanges = async () => {
+        const contactsCopy = {...contacts};
+        try {
+            await axiosOrders.put('/contacts.json', contactsCopy);
+        } finally {
+            setShowEdit(!showEdit)
+        }
+    };
 
     return (
         <div className="container">
@@ -24,6 +41,49 @@ const Contacts = () => {
             <p><b>Zip Code: </b>{contacts.zipcode}</p>
             <p><b>Phone number: </b>{contacts.phone}</p>
             <p><b>Email: </b>{contacts.email}</p>
+            <button type="button" className="btn" onClick={() => setShowEdit(!showEdit)}>Edit</button>
+            {
+                showEdit ?
+                    <>
+                        <p>State: <input
+                            type="text"
+                            name="state"
+                            value={contacts.state}
+                            onChange={changeContacts}
+                        /></p>
+                        <p>City: <input
+                            type="text"
+                            name="city"
+                            value={contacts.city}
+                            onChange={changeContacts}
+                        /></p>
+                        <p>Street: <input
+                            type="text"
+                            name="street"
+                            value={contacts.street}
+                            onChange={changeContacts}
+                        /></p>
+                        <p>Zip Code: <input
+                            type="text"
+                            name="zipcode"
+                            value={contacts.zipcode}
+                            onChange={changeContacts}
+                        /></p>
+                        <p>Phone number: <input
+                            type="text"
+                            name="phone"
+                            value={contacts.phone}
+                            onChange={changeContacts}
+                        /></p>
+                        <p>Email: <input
+                            type="text"
+                            name="email"
+                            value={contacts.email}
+                            onChange={changeContacts}
+                        /></p>
+                        <button type="button" onClick={saveChanges}>Save</button>
+                    </> : null
+            }
         </div>
     );
 };
